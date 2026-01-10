65e9ce4cea70130c68ebb20779c9b1dded34999d
358TECH-CAMP <358hirata@gmail.com>
2026-01-10 16:44:23 +0900

    ベーシック認証本番環境

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,24 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
-   # ★ これを追加
-  before_action :basic_auth, if: -> { Rails.env.production? }
+  before_action :basic_auth, if: -> { Rails.env.production? }  # 本番環境のみ
 
   private
 
   # Basic認証
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
+      username == ENV["BASIC_AUTH_USER"] &&
+      password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
-  
+
   # ログインしているユーザーを取得
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
-  # ログインしていなければログインページにリダイレクト
+  # ログインしていなければログインページへ
   def authenticate_user!
-    unless current_user
-      redirect_to login_path, alert: "ログインしてください"
-    end
+    redirect_to login_path, alert: "ログインしてください" unless current_user
   end
 end
